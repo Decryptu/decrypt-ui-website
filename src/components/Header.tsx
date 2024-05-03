294f7f892324911a22de253a74fba892dcb70935
Decrypt <louis.vicomte@protonmail.com>
2024-05-03 17:57:45 +0200

    open graph config

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,32 @@
 import type React from "react";
 import { Link } from "react-router-dom";
 
+// Import SVG files
+import logoCool from "../assets/images/logo-cool.svg";
+import logoFot from "../assets/images/logo-fot.svg";
+import logoGeist from "../assets/images/logo-geist.svg";
+
 const Header: React.FC = () => {
-	return (
-		<header className="bg-gray-800 text-white p-4">
-			<h1 className="text-lg">
-				<Link to="/" className="hover:underline">
-					Decrypt UI
-				</Link>
-			</h1>
-		</header>
-	);
+  // Function to select a random logo
+  const getRandomLogo = () => {
+    const logos = [logoCool, logoFot, logoGeist];
+    const randomIndex = Math.floor(Math.random() * logos.length);
+    return logos[randomIndex];
+  };
+
+  // Store the random logo in a variable
+  const randomLogo = getRandomLogo();
+
+  return (
+    <header className="bg-decrypt-800 text-white p-4">
+      <h1 className="text-lg">
+        <Link to="/" className="hover:underline">
+          {/* Render the random logo */}
+          <img src={randomLogo} alt="Decrypt Logo" className="w-48"/>
+        </Link>
+      </h1>
+    </header>
+  );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      decrypt: {
+        400: "#757678",
+        500: "#27292B",
+        600: "#1E1F22",
+        700: "#191A1D",
+        800: "#0E0F10",
+        900: "#050506",
+      },
+      blue: "#7BD0F5",
+      pink: "#F9C",
+      white: "#FFF",
+      black: "#000",
+    },
+    extend: {},
+  },
+  plugins: [],
 };
